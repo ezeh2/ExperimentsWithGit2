99da308ced672775d91b2e81fd119cdc8465f173
Edward <e.zeh@gmx.ch>
2022-09-11 12:10:26 +0200

    removed uneeded space in last line

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@
 git merge-file -p  -L a -L b -L c fc.txt fc.txt fc_ab.txt
 
 # conflict
-git merge-file -p  -L a -L b -L c fc_ab.txt fc.txt fc_abc.txt
+# git merge-file -p  -L a -L b -L c fc_ab.txt fc.txt fc_abc.txt
 
 
