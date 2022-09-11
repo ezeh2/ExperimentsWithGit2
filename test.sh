a2b80f39b26193857031f319d076d61d4ea985bd
Edward <e.zeh@gmx.ch>
2022-09-11 11:55:06 +0200

    modifued test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 # no conflict
-git merge-file -p  -L a -L b -L c fc.txt fc.txt fc_ab.txt
+# git merge-file -p  -L a -L b -L c fc.txt fc.txt fc_ab.txt
 
 # conflict
-git merge-file -p  -L a -L b -L c fc_ab.txt fc.txt fc_abc.txt
+# git merge-file -p  -L a -L b -L c fc_ab.txt fc.txt fc_abc.txt
 
+# no conflict
+git merge-file -p  -L a -L b -L c fc.txt fc_ab.txt fc_abc.txt > fc.txt.1
+
+# no conflict, but b at line 2 instead line 3
+git merge-file -p  -L a -L b -L c fc.txt.1 fc_a.txt fc_ab.txt 
 
